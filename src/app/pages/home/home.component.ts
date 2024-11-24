29f992393f5c0ea2966f1ac0ec097ecfb7627d52
Carlos <carlosezequiel19palladino@gmail.com>
2024-11-24 14:49:57 -0300

    cambios en la vista 1024

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProyectosComponent } from '../proyectos/proyectos.component';
 import { ContactoComponent } from '../contacto/contacto.component';
 import { ExperienciaComponent } from "../experiencia/experiencia.component";
+import { CertificacionesComponent } from "../certificaciones/certificaciones.component";
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [ProyectosComponent,
-    ContactoComponent, ExperienciaComponent],
+    ContactoComponent, ExperienciaComponent, CertificacionesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
