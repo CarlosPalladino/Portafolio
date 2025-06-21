cf0c98187ebd67f1636c671364de8373251ca565
Carlos <carlosezequiel19palladino@gmail.com>
2025-06-21 15:19:09 -0300

    Auto-generated commit

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { ProyectosComponent } from '../proyectos/proyectos.component';
 import { ContactoComponent } from '../contacto/contacto.component';
+import { ExperienciaComponent } from "../experiencia/experiencia.component";
+import { CertificacionesComponent } from "../certificaciones/certificaciones.component";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProyectosComponent,
-    ContactoComponent],
+  imports: [
+    ContactoComponent,
+    ExperienciaComponent,
+    CertificacionesComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+scrollToContacto() {
+  const contactoElement = document.getElementById('contacto');
+  if (contactoElement) {
+    contactoElement.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+descargarCV() {
+  const link = document.createElement('a');
+  link.href = 'assets/CV/CarlosPalladinoCv.pdf';
+  link.download = 'CarlosPalladinoCv.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
 
 }
